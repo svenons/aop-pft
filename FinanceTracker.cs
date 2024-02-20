7a28c234d78d7bd8b00ad3e85259242e60a3f7df
svenons <gotvonisvens@gmail.com>
2024-02-20 11:01:30 +0100

    FinanceTracker class and changes to IFinance interface

--- FinanceTracker.cs
+++ FinanceTracker.cs
@@ -1,20 +1,48 @@
 namespace PersonalFinanceTracker {
     public class FinanceTracker: IFinance{
+        public List<Transaction>? Transactions { get; set; }
         public void AddTransaction(Transaction transaction)
         {
-
+            if (Transactions == null)
+            {
+                Transactions = new List<Transaction>();
+            }
+            Transactions.Add(transaction);
         }
-        public void RemoveTransaction(Transaction transaction)
+        public bool RemoveTransaction(Transaction transaction)
         {
-
+            if (Transactions != null)
+            {
+                if(Transactions.Contains(transaction))
+                {
+                    Transactions.Remove(transaction);
+                    return true;
+                }             
+            }
+            return false;
         }
-        public void GetTransactions()
+        public List<Transaction> GetTransactions()
         {
-
+            if (Transactions == null)
+            {
+                Transactions = new List<Transaction>();
+            }
+            return Transactions;
         }
-        public void setFinanceCategory(Transaction transaction, string category)
+        public bool setFinanceCategory(Transaction transaction, string newCategory)
         {
-            
+            if (Transactions != null)
+            {
+                foreach (Transaction t in Transactions)
+                {
+                    if (t.ID == transaction.ID)
+                    {
+                        t.TransactionCategory = (Transaction.Category)Enum.Parse(typeof(Transaction.Category), newCategory);
+                        return true;
+                    }
+                }
+            }
+            return false;
         }
     }
 }
