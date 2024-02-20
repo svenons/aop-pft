add0f6b38f13a81285f92ffdc39b98c18dff7b02
Niklas Braun <niklas.braun30419@gmail.com>
2024-02-20 10:49:05 +0100

    Added More Categories to enum Category

--- Transaction.cs
+++ Transaction.cs
@@ -5,18 +5,19 @@ namespace PersonalFinanceTracker {
         public string Description { get; set; }
         public decimal Amount { get; set; }
         public enum Category {
-              Income,
-              Expense
-       }
+                Income,
+                Rent,
+                Utilities,
+                Groceries,
+                Other
+        }
         public Category TransactionCategory { get; set; }
 
-
-       public Transaction(DateTime date, string description, decimal amount, Category category) {
-           Date = date;
-           Description = description;
-           Amount = amount;
-           TransactionCategory = category;
-       }
-
+        public Transaction(DateTime date, string description, decimal amount, Category category) {
+            Date = date;
+            Description = description;
+            Amount = amount;
+            TransactionCategory = category;
+        }
     }
 }
